67f85403b54c447e0d93d8e839c935654c1d7e34
chris <930193463@qq.com>
2019-07-13 15:24:51 +0800

    qqq

--- src/main/java/com/qf/realm/MyJdbcRealm.java
+++ src/main/java/com/qf/realm/MyJdbcRealm.java
@@ -3,6 +3,7 @@ package com.qf.realm;
 import org.apache.shiro.realm.jdbc.JdbcRealm;
 
 public class MyJdbcRealm extends JdbcRealm {
+
     public MyJdbcRealm(){
         setSaltStyle(SaltStyle.COLUMN);
     }
